e4139f8ce9d4df442b76cf1911bea56e7b9e5e3b
svirus0304 <svirus0304@naver.com>
2016-07-08 10:01:13 +0900

    memberDTO.java, .class

---
+++ src/main/java/com/ryulab/spring/DTO/MemberDTO.java
@@ -0,0 +1,34 @@
+package com.ryulab.spring.DTO;
+
+public class MemberDTO {
+	private String mem_id;
+	private String mem_pw;
+	private String mem_email;
+	private String mem_nickname;
+	
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getMem_pw() {
+		return mem_pw;
+	}
+	public void setMem_pw(String mem_pw) {
+		this.mem_pw = mem_pw;
+	}
+	public String getMem_email() {
+		return mem_email;
+	}
+	public void setMem_email(String mem_email) {
+		this.mem_email = mem_email;
+	}
+	public String getMem_nickname() {
+		return mem_nickname;
+	}
+	public void setMem_nickname(String mem_nickname) {
+		this.mem_nickname = mem_nickname;
+	}
+	
+}
